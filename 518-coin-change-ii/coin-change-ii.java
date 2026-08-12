6ccc0f4e97d89bb7560542abacac8407e0bb1cdf
sahana-2006-code <sahanagundam@gmail.com>
2026-08-12 22:33:52 +0530

    Time: 12 ms (48.35%) | Memory: 56.4 MB (54.65%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<=coins.length;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]<=j){
+                    int in = dp[i][j-coins[i-1]];
+                    int ex = dp[i-1][j];
+                    dp[i][j] = in+ex;
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
